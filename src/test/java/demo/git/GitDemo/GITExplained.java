97a3e6b8f159c082a6cad097560c69718c27880d
srinidhitekarch <training@qtbots.com>
2021-09-15 00:51:27 +0530

    test changes

--- src/test/java/demo/git/GitDemo/GITExplained.java
+++ src/test/java/demo/git/GitDemo/GITExplained.java
@@ -2,9 +2,8 @@ package demo.git.GitDemo;
 
 public class GITExplained {
 /*
- ignore files 
  README.md  ---> what all teh pre-requisite 
- 				
+ ignore files  --> ignore is completed  				
  
  * */
 }
