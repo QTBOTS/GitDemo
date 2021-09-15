4cd86f8d2e1491231f92d8cd6c2c84b7d3787417
srinidhitekarch <training@qtbots.com>
2021-09-16 00:04:49 +0530

    adding notes

--- src/test/java/demo/git/GitDemo/GITExplained.java
+++ src/test/java/demo/git/GitDemo/GITExplained.java
@@ -2,6 +2,7 @@ package demo.git.GitDemo;
 
 public class GITExplained {
 /*
+ 
  README.md  ---> what all teh pre-requisite 
  ignore files  --> ignore is completed  				
  git status  --> status of the project. (any unsaved files. any un commited changes, can see branch name)
@@ -9,6 +10,22 @@ public class GITExplained {
  git commit -m "message you want to name" --> to save the changes to git repo
  git push origin master --> to push the code from local to cloud (remote)
  git pull origin master --> to take the changes form cloud to local
+ 
+ git branch <branchname>  --> it will create an branch with its name. Code will be taken from last branch (branch name before creating)
+ 
+ Master -->
+ Sri
+ Abhi
+ jo
+ ran
+ 
+ git checkout <branchname>  ---> git will shift or change the branch name
+ git branch  --> it will list down the branches which is available locally
+ git branch -d <branchname> --> to delete the branch locally
+ git branch -r  --> to list down the branch which is there in origin
+ 
+ Best practice :--> as soon as you create an branch good to push it immediatly
+ 
  
  * */
 }
