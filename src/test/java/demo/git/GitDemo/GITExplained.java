db77e20228acc26835f2be4e3e737bd42477f47e
srinidhitekarch <training@qtbots.com>
2021-09-16 00:02:12 +0530

    adding notes

--- src/test/java/demo/git/GitDemo/GITExplained.java
+++ src/test/java/demo/git/GitDemo/GITExplained.java
@@ -4,6 +4,11 @@ public class GITExplained {
 /*
  README.md  ---> what all teh pre-requisite 
  ignore files  --> ignore is completed  				
- git status  --> status of the project. (any unsaved files. any un commited changes
+ git status  --> status of the project. (any unsaved files. any un commited changes, can see branch name)
+ git add <filename> ---> to save the file (* represent "all files", git add .gitignore)
+ git commit -m "message you want to name" --> to save the changes to git repo
+ git push origin master --> to push the code from local to cloud (remote)
+ git pull origin master --> to take the changes form cloud to local
+ 
  * */
 }
