96d57dcc6b2d6d47055d93dbde547ba89735b145
srinidhitekarch <training@qtbots.com>
2021-09-15 23:44:39 +0530

    changes done for git ignore

--- src/test/java/demo/git/GitDemo/GITExplained.java
+++ src/test/java/demo/git/GitDemo/GITExplained.java
@@ -4,6 +4,6 @@ public class GITExplained {
 /*
  README.md  ---> what all teh pre-requisite 
  ignore files  --> ignore is completed  				
- 
+ git status  --> status of the project. (any unsaved files. any un commited changes
  * */
 }
